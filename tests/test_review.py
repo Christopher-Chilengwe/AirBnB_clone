c9971d29b721e2e87013390514729519674ac644
Christopher Chilengwe <christopherchilengwe@gmail.com>
2024-05-12 14:45:50 +0200

    Tests

--- tests/test_review.py
+++ tests/test_review.py
@@ -3,43 +3,47 @@
 
 
 import unittest
-from models.amenity import Amenity
+from models.review import Review
 from models.base_model import BaseModel
 from datetime import datetime
 from uuid import UUID
 from models import storage
 
 
-class TestsAmenity(unittest.TestCase):
+class TestsBaseModel(unittest.TestCase):
+    """class test base model for unittest"""
 
-    obj = Amenity()
+    obj = Review()
 
-    def setUp(self):
+    def seUp(self):
         """set initial"""
-        name = ""
+        place_id = ""
+        user_id = ""
+        text = ""
 
-    def test_normal_cases_amenity(self):
+    def test_normal_cases_review(self):
         """normal cases"""
-        my_object = Amenity()
+        my_object = Review()
         my_object.name = "Holbiland"
         my_object.my_number = 29
         my_object.save()
         my_object_dict = my_object.to_dict()
         self.assertEqual(my_object.name, "Holbiland")
         self.assertEqual(my_object.my_number, 29)
-        self.assertEqual(my_object.__class__.__name__, "Amenity")
+        self.assertEqual(my_object.__class__.__name__, "Review")
         self.assertEqual(isinstance(my_object.created_at, datetime), True)
         self.assertEqual(isinstance(my_object.updated_at, datetime), True)
         self.assertEqual(type(my_object.__dict__), dict)
 
     def test_subclass(self):
         """test if class is subclass"""
-        self.assertEqual(issubclass(Amenity, BaseModel), True)
+        self.assertEqual(issubclass(Review, BaseModel), True)
 
     def test_type(self):
         """test type of object"""
-        obj = Amenity()
-        self.assertEqual(type(self.obj.name), str)
+        self.assertEqual(type(self.obj.place_id), str)
+        self.assertEqual(type(self.obj.user_id), str)
+        self.assertEqual(type(self.obj.text), str)
 
 
 if __name__ == "__main__":
